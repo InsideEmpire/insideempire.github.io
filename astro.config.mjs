bd4c738264adb3fa52a64db0bbc53900113479c8
Garrison <garrison.liu@outlook.com>
2026-04-02 20:34:15 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://insideempire.github.io",
-  base: "insideempire.github.io",
+  base: "/insideempire.github.io",
   vite: {
     plugins: [tailwindcss()],
   },
